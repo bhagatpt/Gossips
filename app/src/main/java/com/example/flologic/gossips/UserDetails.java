6f674b141c8abf2d222309faea182171ee1eaa49
Pradnya <bhagatpt@gmail.com>
2018-02-19 15:32:04 +0530

    Test

---
+++ app/src/main/java/com/example/flologic/gossips/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.flologic.gossips;
+
+/**
+ * Created by Parag on 2/16/2018.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
